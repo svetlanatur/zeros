e8caa94618bf4a199cf8918360cdb00c53a60c4e
Svetlana Tur <svetaltur@gmail.com>
2018-02-23 21:55:58 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+var sum=0;
+var i=1;
+while (number/Math.pow(5,i)>=1){
+  sum=sum+(number-number%Math.pow(5,i))/Math.pow(5,i);
+  i++;
+}
+return sum;
 }
